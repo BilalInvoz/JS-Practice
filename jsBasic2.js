a04f88d08c8041b81088d58fb688646ac701f9ab
BilalInvoz <103623977+BilalInvoz@users.noreply.github.com>
2022-04-14 16:40:23 +0500

    Day 2

---
+++ jsBasic2.js
@@ -0,0 +1,14 @@
+////////////// if else statements ///////////
+let num = 10;
+for(let i = 1; i <= num; i++){
+    if(i == 3){
+        console.log("Try")
+    }
+    else if(i == 7){
+        console.log("Catch")
+    }
+    else{
+        console.log("Try Catch")
+    }
+
+}
